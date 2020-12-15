155a09a7a173f211615c2044b0158385989d026c
James Paige <Bob@HamsterRepublic.com>
2020-12-15 08:54:30 -0500

    Rebase ohrrpgce docker files on Debian 9 LTS, and move the compile script to the ohrrpgce-bin image, and don't auto-run it

--- ohrrpgce-bin/compile_ohrrpgce_nightly_inside_docker.sh
+++ ohrrpgce-bin/compile_ohrrpgce_nightly_inside_docker.sh
@@ -4,7 +4,8 @@ cd /ohrrpgce
 
 if [ ! -d "wip" ] ; then
   echo "Check out a fresh working copy of the ohrrpgce wip branch"
-  svn co http://rpg.hamsterrepublic.com/source/wip wip
+  svn co --non-interactive --trust-server-cert-failures=unknown-ca \
+    http://rpg.hamsterrepublic.com/source/wip wip
   if [ ! -d "wip" ] ; then
     echo "Unable to check out ohrrpgce wip branch"
     exit 1
